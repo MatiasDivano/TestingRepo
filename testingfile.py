ea435004a981d822eb9798428971fe47e31d27e1
MatiasDivano <110577173+MatiasDivano@users.noreply.github.com>
2022-08-10 21:47:22 -0300

    Update testingfile.py

--- testingfile.py
+++ testingfile.py
@@ -1,4 +1,11 @@
+class persona(object):
+    def __init__(self, name, surn, dni):
+        self.dni = dni
+        self.name = name
+        self.surn = surn
+
 # repaso de usos de listas y de for
+# todo print fue probado en vs
 
 lista = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 
